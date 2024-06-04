aa959ce8e4e288d3b20006e80f0c28a58ed89a9b
johan22ch <johanchumbi16@gmail.com>
2024-06-04 11:33:45 -0500

    Update EjercicioUnoDoWhile.c

--- EjercicioUnoDoWhile.c
+++ EjercicioUnoDoWhile.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    int num, sum = 0, i = 1;
-
-    // Ingrese un número entero
-    printf("Ingresa un numero: ");
-    scanf("%d", &num);
-
-    // Se suman de todos los números anterioes hasta num usando un bucle do-while
+int sumaDigitos(int numero) {
+    int suma = 0;
     do {
-        sum += i;
-        i++;
-    } while (i <= num);
+        suma += numero % 10;
+        numero = numero / 10;
+    } while (numero != 0);
+    return suma;
+}
 
-    // Imprimir el resultado
-    printf("La suma de los numeros desde 1 hasta %d es %d\n", num, sum);
-    printf("Muchas gracias mundo :D\n");
+int main() {
+    int numero = 12567;
+    int resultado = sumaDigitos(numero);
+    printf("La suma de los dígitos de %d es: %d\n", numero, resultado);
     return 0;
 }
