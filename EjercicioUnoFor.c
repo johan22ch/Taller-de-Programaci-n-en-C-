6659eec85d58ad0f970bdfa1e218d24d9df14876
johan22ch <johanchumbi16@gmail.com>
2024-06-04 11:30:11 -0500

    Update EjercicioUnoFor.c

--- EjercicioUnoFor.c
+++ EjercicioUnoFor.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-int main() {
-    int num, sum = 0;
 
-    // Ingrese un número entero
-    printf("Ingresa un numero: ");
-    scanf("%d", &num);
-
-    // Se suman de todos los números anteriores hasta num
-    for (int i = 1; i <= num; i++) {
-        sum += i;
+int sumaDigitos(int numero) {
+    int suma = 0;
+    while(numero != 0) {
+        suma += numero % 10;
+        numero = numero / 10;
     }
+    return suma;
+}
 
-    // Imprimir el resultado
-    printf("La suma de los digitos desde 1 hasta %d es %d\n", num, sum);
-    printf("Muchas gracias mundo :D\n");
+int main() {
+    int numero = 12567;
+    int resultado = sumaDigitos(numero);
+    printf("La suma de los dígitos de %d es: %d", numero, resultado);
     return 0;
 }
